29df8cf70f8be5ee036918f3fd8fd738d9b789b0
Imanuel Costigan <i.costigan@me.com>
2020-01-16 20:17:03 +1100

    Enable multi-path simulations

--- sim.R
+++ sim.R
@@ -1,17 +1,44 @@
 library(tidyverse)
 
 
-gbm <- function(x0, mu, sigma, step_size, n_steps) {
-  crn <- cumsum(rnorm(n_steps, 0, 1))
-  drift <- mu * n_steps * step_size
-  diffusion <- sigma * sqrt(step_size)
-  x0 * exp(drift + diffusion * crn)
+gbm <- function(x0, mu, sigma, step_size, n_steps, n_paths) {
+
+  rn_paths <- n_paths %>%
+    rerun(
+      rnorm(n_steps)
+    ) %>%
+    map(
+      ~ x0 * exp(n_steps * step_size * mu + sqrt(step_size) * sigma * cumsum(.))
+    )
 }
 
 
-# single path
+# global variables
 
-gbm(1, 0.1, 0.5, 1/12, 5 * 12)
+x0 <- 1
+step_size <- 1 / 12
+n_steps <- 5 * 12
+n_paths <- 100
+
+# single scenario
+
+system.time(res_single_scenario <- gbm(x0, 0, 0.5, step_size, n_steps, n_paths))
+
+# multiple scenarios
+system.time({
+  res_multi_scenarios <- list(
+    mu = seq(-0.05, 0.05, length.out = 10),
+    sigma = seq(0.1, 1.0, length.out = 10)
+  ) %>%
+    cross_df() %>%
+    pmap(gbm, x = 1, step_size, n_steps, n_paths)
+})
+
+
+# XVA scenario
+
+
+
 
 
 
